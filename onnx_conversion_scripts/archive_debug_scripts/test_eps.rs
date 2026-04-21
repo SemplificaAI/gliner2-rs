da238a31e1f27b82ee889254bd74c0b688e84804
Dario Finardi <df@dariofinardi.it>
2026-04-21 15:54:02 +0200

    chore: untrack debug scripts and add to gitignore

--- onnx_conversion_scripts/archive_debug_scripts/test_eps.rs
+++
@@ -1,7 +0,0 @@
-use ort::execution_providers::*;
-fn main() {
-    let _ = QNNExecutionProvider::default();
-    let _ = OpenVINOExecutionProvider::default();
-    let _ = CoreMLExecutionProvider::default();
-    let _ = XNNPACKExecutionProvider::default();
-}
